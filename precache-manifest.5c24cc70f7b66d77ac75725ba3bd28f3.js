3bee2e75c14f1a1a6ce3d75c3f3c6c19788bfa98
kjduncan <katelynduncan@Katelyns-MacBook-Pro.local>
2018-11-30 23:27:00 -0500

    Updates

---
+++ precache-manifest.5c24cc70f7b66d77ac75725ba3bd28f3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "efbf27d8f78f10379358",
+    "url": "/static/css/main.49e756ce.chunk.css"
+  },
+  {
+    "revision": "efbf27d8f78f10379358",
+    "url": "/static/js/main.efbf27d8.chunk.js"
+  },
+  {
+    "revision": "7b4e03f3089cff08d1e6",
+    "url": "/static/js/1.7b4e03f3.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f8aa06fe584330c7438c571c87cf4d05",
+    "url": "/static/media/Mercury.f8aa06fe.svg"
+  },
+  {
+    "revision": "ae9d955f6f0cd9342a33f993c0910125",
+    "url": "/static/media/Venus.ae9d955f.svg"
+  },
+  {
+    "revision": "ba2e9d1dd4fce4b98a3f5a3509155238",
+    "url": "/static/media/Earth.ba2e9d1d.svg"
+  },
+  {
+    "revision": "4ab39eba28585b3c05d784c5f0c8d95c",
+    "url": "/static/media/mars.4ab39eba.svg"
+  },
+  {
+    "revision": "5b6a42ccc3cbbfb8afe4ba6a7d8b8b73",
+    "url": "/static/media/jupiter.5b6a42cc.svg"
+  },
+  {
+    "revision": "3ebd2023796e7a17f8d3076c587e5e87",
+    "url": "/static/media/saturn.3ebd2023.svg"
+  },
+  {
+    "revision": "970afb863f72766b16120fbf3cbdab48",
+    "url": "/static/media/Uranus.970afb86.svg"
+  },
+  {
+    "revision": "17aac410943eb151a63283255459d447",
+    "url": "/static/media/neptune.17aac410.svg"
+  },
+  {
+    "revision": "47738ae57751fefd47244c31a14b767f",
+    "url": "/static/media/sun.47738ae5.svg"
+  },
+  {
+    "revision": "bbd6b63b84d229a8765f7b34e94ecc92",
+    "url": "/static/media/spaceshipFlame.bbd6b63b.svg"
+  },
+  {
+    "revision": "5babd20dc2f79ab1da0082dc772191d6",
+    "url": "/index.html"
+  }
+];
